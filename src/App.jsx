b4d987e51e9a757d9f9cb17e8f6fc9168b0f3653
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-11-16 17:47:05 +0100

    prueba tecnica finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState("")
+  const [img, setImg] = useState("")
+
+
+  useEffect(() => {
+  axios.get('https://catfact.ninja/fact').then((response)=>setFact(response.data.fact))
+    }
+  , [])
+  
+  let slicedFact= fact.split(' ').slice(0,3).join('+')
+  const API_KEY="0Tb9bU13tK0i1mu3Cto4g70WNxw28VH3"
+
+  useEffect(() => {
+    axios.get(` https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${slicedFact}`).then((response)=>setImg(response.data.data[0].images.original.url))
+      }
+    , [fact])
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Prueba Tecnica 01 Midudev Carlos Calleja </h1>
+    <div className='container'>
+      <h2>{fact}</h2>
+      {/* <h2>{img}</h2> */}
+      
+      <img src={img}/> 
+    </div>
     </>
   )
 }
